72b1b176dcaad7eea67f5130f62f5a8baaf5ded8
Luna Tsuki <lunax@mlunax.com>
2021-01-09 06:13:26 +0100

    colorize linux-install script

--- linux-install.sh
+++ linux-install.sh
@@ -8,8 +8,9 @@ SCRIPTS_DIR=${HOME}/.config/mpv/scripts
 MPVDSC_DIR=${HOME}/.config/mpv/node-scripts/mpv-dsc-ipc
 
 if [ -d $MPVDSC_DIR ]; then
-    read -p "Detected mpv-dsc-ipc directory in mpv config. Do you want to overwrite scripts? [y/N] " -n 1 -r
-    echo
+    echo -e "\e[31mDetected mpv-dsc-ipc directory in mpv config."
+    read -p "Do you want to overwrite scripts? [y/N] " -n 1 -r
+    echo -e "\e[0m"
     if [[ ! $REPLY =~ ^[Yy]$ ]]; then
         exit 1
     fi
@@ -25,6 +26,7 @@ cp ./discordrpc.js $SCRIPTS_DIR
 cd $MPVDSC_DIR
 npm i
 
-echo "---"
-echo "Remeber to configure your config: "
-echo "input-ipc-server=~/.config/mpv/mpv.sock"
+echo -e "\e[32m-----------------"
+echo -e "$(tput bold)Remember to add to your config: "
+echo -e "input-ipc-server=~/.config/mpv/mpv.sock$(tput sgr0)"
+echo -e "\e[32m-----------------\e[0m"
